dfcd3b8f60905a229f69329b7d6e850abf3e57c4
Patrick McDonagh <patrickjmcd@gmail.com>
2020-06-16 22:48:00 -0500

    adds working prometheus and unifi

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # scp -r patrickjmcd@192.168.1.252:/volume1/kube-secrets/secrets .
 kubectl create ns drone
 # kubectl create ns openfaas-fn
@@ -5,8 +6,11 @@ kubectl create ns monitoring
 kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.14/deploy/manifests/00-crds.yaml
 kubectl apply -f storage/ -f secrets/
 helmfile apply
-kubectl apply -f cert-manager/
+
 # kubectl apply -f openfaas/
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.1/aio/deploy/alternative.yaml  
 kubectl apply -f dashboard/
 kubectl apply -f cronjobs/
+kubectl apply -f unifi/
+sleep 5m
+kubectl apply -f cert-manager/
